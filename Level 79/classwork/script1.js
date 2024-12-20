563a9a67a680d13ef8e571de07db2a135395a662
sannyy1 <alex.maisuradze29@gmail.com>
2024-12-20 05:47:40 -0800

    commit

---
+++ Level 79/classwork/script1.js
@@ -0,0 +1,9 @@
+console.log('This is a text');
+console.log(7 - 10);
+console.log(10 + 10)
+console.log(10 * 10)
+console.log(10 / 2)
+console.log(5 ** 2)
+
+
+console.log('გამარჯობა მე ვარ ' + (15 + 1) + ' წლის' + ' და მალე გავხდები ' + (16 + 1) + ' წლის')
---
+++ Level 79/homework/script.js
@@ -0,0 +1,83 @@
+// 1. alert - ის და console.log - ის გამოყენებით, გააკეთეთ 10-10 მაგალითი, ყველა მათემათიკურ ოპერატორზე(+,-,*,/,**)
+
+
+alert(10 + 10)
+alert(100 + 10)
+alert(10 - 10)
+alert(10 - 1)
+alert(10 * 10)
+alert(10 * 3)
+alert(10 ** 2)
+alert(10 ** 3)
+alert(10 / 2)
+alert(10 / 5)
+
+// console log
+
+console.log(10 + 10)
+console.log(100 + 10)
+console.log(10 - 10)
+console.log(10 - 1)
+console.log(10 * 10)
+console.log(10 * 3)
+console.log(10 ** 2)
+console.log(10 ** 3)
+console.log(10 / 2)
+console.log(10 / 5)
+
+
+// 2. alert - ის და console.log - ის გამოყენებით, ორი სიტყვა შეართეთ ერთმანეთს
+ 
+alert("Sandro" + " Maisuradze")
+
+console.log("Sandro" + " Maisuradze")
+
+
+// 3. alert - ის და console.log - ის გამოყენებით, შექმენით შემდეგი სტრუქტურა. The world will be destroyed in 10 seconds, გამოიტანეთ იგივე ტექსტი 0 ის ჩათვლთ.
+
+alert("The world will be destroyed in 10 seconds")
+alert("The world will be destroyed in 9 seconds")
+alert("The world will be destroyed in 8 seconds")
+alert("The world will be destroyed in 7 seconds")
+alert("The world will be destroyed in 6 seconds")
+alert("The world will be destroyed in 5 seconds")
+alert("The world will be destroyed in 4 seconds")
+alert("The world will be destroyed in 3 seconds")
+alert("The world will be destroyed in 2 seconds")
+alert("The world will be destroyed in 1 seconds")
+alert("The world will be destroyed in 0 seconds")
+
+
+
+console.log("The world will be destroyed in 10 seconds")
+console.log("The world will be destroyed in 9 seconds")
+console.log("The world will be destroyed in 8 seconds")
+console.log("The world will be destroyed in 7 seconds")
+console.log("The world will be destroyed in 6 seconds")
+console.log("The world will be destroyed in 5 seconds")
+console.log("The world will be destroyed in 4 seconds")
+console.log("The world will be destroyed in 3 seconds")
+console.log("The world will be destroyed in 2 seconds")
+console.log("The world will be destroyed in 1 seconds")
+console.log("The world will be destroyed in 0 seconds")
+
+
+// 4. კომენტარების დახმარებით ახსენით თუ რა არის alert()
+// alert() არის გაფრთხილება რომლის გამოყენების შემთხვევაში ეკრანზე გამოიტანს გამაფრთხილებელ ფანჯარას სადაც ეწერება ის ტექსტი რომელი ტექსტიც იქნა ჩაწერილი ალერტის შემდეგ არსებულ ფრჩხილებში.
+
+
+
+// 5. კომენტარების დახმარებით ახსენით თუ რა არის console.log()
+// console.log() არის თითქმის მსგავსი რამ რაც ალერტი უბრალოდ ალერტისგან განსხვავებით კონსოლ ლოგი თვითონ ეკრანზე მარჯვენა ღილაკის დაწკაპებით გამოტანილ ფანჯარაში დავინახავთ ჩვენ საბოლოო შედეგს (inspect - ის საშუალებით)
+
+
+
+// 6. კომენტარების დახმარებით ახსენით თუ რა არის JS და რისთვის გამოიყენება იგი.
+// javascript არის ერთ-ერთი გავრცელებული პროგრამირების ენა რომელიც გვაძლევს იმის საშუალებას რომ ჩვენს მიერ აწყობილი საიტი გავხადოთ ინტერაქციული და მივცეთ საიტს სრულყოფილი სახე.
+
+
+
+// 7. წავიკითხე დამატებით მასალა
+
+
+// 8. ვუყურე ვიდეოს.
