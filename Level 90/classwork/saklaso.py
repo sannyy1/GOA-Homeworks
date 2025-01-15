a406842f3aab57dbb20669d5def1b3b30338d785
sannyy1 <alex.maisuradze29@gmail.com>
2025-01-15 01:31:31 -0800

    commit

---
+++ Level 90/classwork/saklaso.py
@@ -0,0 +1,16 @@
+# შექმენით თითოეული, ანუ list1, tuple1, set1 ცვლადები
+
+list1 = [1, 2, 3, 4, 5]
+print(list1)
+
+
+tuple1 = (1, 2, 3, 4, 5)
+print(tuple1)
+
+
+set1 = {1, 2, 3, 4, 5}
+print(set1)
+
+
+set1 = {1, 2, 2, 3}
+print(set1)
